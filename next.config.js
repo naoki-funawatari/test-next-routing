8ca295bd9153a9e45fab3b8d01f6b5511d7b849d
naoki-funawatari <g011c1180@gmail.com>
2021-11-11 09:51:26 +0900

    feat: 下準備。

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./src/"),
+    };
+
+    return config;
+  },
+};
